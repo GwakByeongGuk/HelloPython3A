4ab8795a5ad44b3b0c9d26054a16cdf7bf2b52a3
GwakByeongGuk <teereal@naver.com>
2024-07-10 17:49:06 +0900

    연산자 개요 산술연산자 실습

---
+++ example/example02.py
@@ -0,0 +1,25 @@
+# 4 수학식을 파이썬 표현식으로 바꿔보아라
+x = 1
+result = 3*x
+
+y=2
+result2 = 3*x+y
+
+result3 = (x+y)/7
+result4 = (3*x+y)/(x+2)
+
+s0 = 0
+v0 = 10
+t = 5
+g = 9.8
+s = s0+v0*t+0.5*g*t**2
+# 12 생년월일을 이용해서 나이를 계산하는 프로그램을 작성하여라
+from datetime import datetime
+
+def birthday(birthdate) :
+    today = datetime.today()
+
+
+# 14
+
+# 16
