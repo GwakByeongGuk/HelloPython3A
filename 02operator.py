a5c13576b7f60af470de7531c0806a41e77dd0ed
GwakByeongGuk <teereal@naver.com>
2024-07-10 14:12:21 +0900

    연산자 개요 산술연산자 실습

--- 02operator.py
+++ 02operator.py
@@ -10,9 +10,57 @@
 # 자료형 승격 promotion
 
 # 매출액 입력 시 총합 출력
-num1 = input('1월 매출은?')
-num2 = input('2월 매출은?')
-num3 = input('3월 매출은?')
+num1 = input('1월 매출 :')
+num2 = input('2월 매출 :')
+num3 = input('3월 매출 :')
+totalSale = {int(num1)+int(num2)+int(num3)}
 print(f'''
-1분기 전체 매출 {int(num1)+int(num2)+int(num3)}원
+1월 매출 {num1}
+2월 매출 {num2}
+3월 매출 {num3}
+1분기 전체 매출 : {totalSale}원
+''')
+# 1분기 수익 계산
+num1 = input('1분기 매출 :')
+num2 = input('1분기 매입 :')
+profit = int(num1) - int(num2)
+print(f'''
+1분기 매출 {num1}
+1분기 매입 {num2}
+수익 : {profit}원
+''')
+# 방의 넓이 구하기
+num1 = input('가로 :')
+num2 = input('세로 :')
+area = int(num1)*int(num2)
+print(f'''
+가로 길이 : {num1}
+세로 길이 : {num2}
+넓이 : {area} ㎠ 
+''')
+# 신체질량지수BMI 구하기
+num1 = input('몸무게 :')
+num2 = input('신장 :')
+BMI = float(num1)/(float(num2)**2)
+print(f'''
+몸무게(kg) : {num1}
+신장(m) : {num2}
+BMI : {BMI}
+''')
+# 홀짝 게임
+num = input('손 안에 동전 수를 입력하세요. :')
+print(f'''
+{int(num)%2}
+''')
+# 빵 나누기
+num1 = input('빵의 갯수:')
+num2 = input('인당 나누어 줄 빵의 갯수:')
+print(f'''
+빵을 나누어 줄 수 있는 학생 수 : {int(num1)//int(num2)}
+남은 빵 개수 : {int(num1)%int(num2)}
+''')
+# 전염병 예상 감염자 구하기
+num = input('경과 일수:')
+print(f'''
+{num}일 이후 예상 감염자 수 : {2**int(num)}명
 ''')
