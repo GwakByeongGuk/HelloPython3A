ced473ff9d26d0ba2269fecb36745cd22fa7a0fd
GwakByeongGuk <teereal@naver.com>
2024-07-19 09:35:14 +0900

    실습문제 풀이

--- example/example04.py
+++ example/example04.py
@@ -158,3 +158,33 @@ price = int(input('지불해야 할 금액은? '))
 paid = int(input('받은 금액은? '))
 compute_charge(price,paid)
 
+# 26 - 세금 계산 computeTax
+
+def computeTax():
+    isMarried = int(input('결혼여부는? 0:미혼, 1:기혼'))
+    salary = int(input('연봉은? '))
+    if isMarried == 0:
+        tax = salary * 0.1
+        if salary >= 3000:
+            tax = salary * 0.25
+    elif isMarried ==1:
+        tax = salary * 0.15
+        if salary >= 6000:
+            tax = salary * 0.35
+
+    print(f'''
+    결혼여부 : {isMarried}, 연봉 : {salary:,} 세금 : {tax:,}
+    ''')
+computeTax()
+
+# 27 - 윤년 구분 isLeapYear
+
+isLeap = '윤년아님!'
+def isLeapYear():
+    year = int(input('년도는? '))
+    cond1 = (year % 4 == 0 and year % 100 != 0)
+    cond2 = (year % 400 == 0)
+    if cond1 or cond2: isLeap = '윤년'
+
+    print(f'{year} 년은 {isLeap}')
+isLeapYear()
