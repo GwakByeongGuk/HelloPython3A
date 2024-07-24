de8e4f389013ef602b6da80fd4e8db3bdab36815
GwakByeongGuk <teereal@naver.com>
2024-07-24 14:05:40 +0900

    성적처리 프로그램 v8b - mariadb로 변경

--- teereal/oop/dao.py
+++ teereal/oop/dao.py
@@ -1,13 +1,20 @@
 # 성적 DAO 클래스
-import sqlite3
+import pymysql
 
 from teereal.oop.models import SungJuk
 
 class SungJukDAO:
+    # 데이터베이스 연결 문자열
+    host = '3.35.133.116'
+    dbname = 'clouds2024'
+    user = 'clouds2024'
+    passwd = 'clouds2024'
+
     # 데이터베이스 객체와 커서 생성
     @staticmethod
     def _make_conn():
-        conn = sqlite3.connect('db/python.db')
+        conn = pymysql.connect(host=SungJukDAO.host, user=SungJukDAO.user,
+                               password=SungJukDAO.passwd, database=SungJukDAO.dbname, charset='utf8')
         cursor = conn.cursor()
         return conn, cursor
 
@@ -19,8 +26,13 @@ class SungJukDAO:
 
     @staticmethod
     def insert_sungjuk(sj):
+        """
+        입력한 성적데이터를 sungjuk 테이블에 저장
+        :param sj: 테이블에 저장할 성적데이터
+        :return cnt: 테이블에 성공적으로 저장된 데이터 건수
+        """
         sql = 'insert into sungjuk (name, kor, eng, mat, tot, avg, grd) \
-               values (?,?,?,?, ?,?,?)'
+               values (%s,%s,%s,%s, %s,%s,%s)'
         conn,cursor = SungJukDAO._make_conn()
         params = (sj.name,sj.kor,sj.eng,sj.mat,sj.tot,sj.avg,sj.grd)
         cursor.execute(sql, params)
@@ -31,6 +43,10 @@ class SungJukDAO:
 
     @staticmethod
     def select_sungjuk():
+        """
+        sungjuk 테이블에서 모든 성적 데이터(번호/이름/국어/영어/수학/등록일)를 읽어옴
+        :return: 조회된 성적데이터 객체
+        """
         sjs = []
         sql = 'select sjno,name,kor,eng,mat,substr(regdate,0,11) from sungjuk'
         conn,cursor = SungJukDAO._make_conn()
@@ -46,7 +62,12 @@ class SungJukDAO:
 
     @staticmethod
     def selectone_sungjuk(sjno):
-        sql = 'select * from sungjuk where sjno = ?'
+        """
+        sungjuk 테이블에서 특정 학생의 성적데이터를 읽어옴
+        :param sjno: 조회할 학생 번호
+        :return: 조회된 성적데이터 객체
+        """
+        sql = 'select * from sungjuk where sjno = %s'
         conn,cursor = SungJukDAO._make_conn()
         params = (sjno,)
         cursor.execute(sql, params)
@@ -65,6 +86,11 @@ class SungJukDAO:
 
     @staticmethod
     def update_sungjuk(sj):
+        """
+        기존에 있던 성적데이터를 새로 입력한 성적데이터로 바꿔 테이블에 저장
+        :param sj:
+        :return:
+        """
         sql = 'update sungjuk set kor=?, eng=?, mat=?, tot=?, avg=?, grd=? \
            where sjno=?'
         conn,cursor = SungJukDAO._make_conn()
